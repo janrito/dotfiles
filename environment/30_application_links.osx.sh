4ac02a45d2b062dc0412e2e975e978fa22413903
janrito <alejandro.giacometti@gmail.com>
2013-06-28 14:41:26 +0100

    sublimetext3 beta support

--- environment/30_application_links.osx.sh
+++ environment/30_application_links.osx.sh
@@ -3,3 +3,7 @@
 
 # link sublimetext user directory to the dotfiles repo
 ln -sfn $HOME/.dotfiles/preferences/sublimetext2/ $HOME/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
+
+
+# link sublimetext user directory to the dotfiles repo
+ln -sfn $HOME/.dotfiles/preferences/sublimetext3/ $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
