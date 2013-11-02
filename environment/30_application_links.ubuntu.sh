fdda83847898919d7a069d81a775784fbd41a3dc
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2013-11-02 00:25:32 +0000

    30 only ubuntu

--- environment/30_application_links.ubuntu.sh
+++ environment/30_application_links.ubuntu.sh
@@ -1,5 +1,5 @@
-# Ubuntu or Debian only stuff. Abort if not Ubuntu.
-[[ "$(cat /etc/issue 2> /dev/null)" =~ Ubuntu || "$(cat /etc/issue 2> /dev/null)" =~ Debian ]] || return 1
+# Ubuntu only stuff. Abort if not Ubuntu.
+[[ "$(cat /etc/issue 2> /dev/null)" =~ Ubuntu ]] || return 1
 
 # link sublimetext user directory to the dotfiles repo
 ln -sfn $HOME/.dotfiles/preferences/sublimetext2/ $HOME/.config/sublime-text-2/User
