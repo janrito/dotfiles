94f6daf6c1559cefefdd09332c54fda235d0def9
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2014-02-18 17:35:13 +0000

    word diff alias utility function

--- environment/aliases.sh
+++ environment/aliases.sh
@@ -4,3 +4,8 @@ alias t=todo.sh
 alias comatlab='matlab -nodisplay -nojvm'
 alias pipu="pip freeze | cut -d = -f 1 | xargs -n 1 pip search | grep -B2 'LATEST:'"
 alias tp='tmuxp'
+
+function wd() {
+    git wdiff "$@"
+    #git diff --no-index --patience --word-diff=plain "$@"
+}
