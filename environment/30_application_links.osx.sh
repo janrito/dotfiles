fdbaf056d71e17d5b13c43e5c6ea82b03d442db0
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2014-02-18 12:08:39 +0000

    preferences for Mou and Taskpaper

--- environment/30_application_links.osx.sh
+++ environment/30_application_links.osx.sh
@@ -7,5 +7,11 @@ ln -sfn $HOME/.dotfiles/preferences/sublimetext2/ $HOME/Library/Application\ Sup
 # link sublimetext user directory to the dotfiles repo
 ln -sfn $HOME/.dotfiles/preferences/sublimetext3/ $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 
+# link Mou user directory to the dotfiles repo
+ln -sfn $HOME/.dotfiles/preferences/Mou/ $HOME/Library/Application\ Support/Mou
+
+# link TaskPaper user directory to the dotfiles repo
+ln -sfn $HOME/.dotfiles/preferences/TaskPaperThemes/ $HOME/Library/Application\ Support/TaskPaper/Themes
+
 # functions
 function byw() { open -a /Applications/Byword.app "$@" ;}
