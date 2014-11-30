3638ce8fe32aee54ec89c1329a85e3a1fdffe261
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2014-11-30 20:16:45 +0000

    run powerline daemon

--- environment/powerline.sh
+++ environment/powerline.sh
@@ -1,5 +1,8 @@
 #tmux powerline
 if check_python && [[ -e "${PYTHON_PACKAGES}/powerline/bindings/bash/powerline.sh" ]]
   then
+    powerline-daemon -q
+    POWERLINE_BASH_CONTINUATION=1
+    POWERLINE_BASH_SELECT=1
     . "${PYTHON_PACKAGES}/powerline/bindings/bash/powerline.sh"
 fi
