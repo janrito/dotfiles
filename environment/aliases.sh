dd865527b4e446a68950b6f5488306df083c1b6d
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2015-09-23 18:17:48 +0100

    get rid of old aliases

--- environment/aliases.sh
+++ environment/aliases.sh
@@ -1,8 +1,6 @@
 
 # aliases
 alias t=todo.sh
-alias comatlab='matlab -nodisplay -nojvm'
-alias pipu="pip freeze | cut -d = -f 1 | xargs -n 1 pip search | grep -B2 'LATEST:'"
 
 function wd() {
     git wdiff "$@"
