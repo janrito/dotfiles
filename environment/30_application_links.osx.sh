9874a28164d54c420b4ec0e87ee1ae98e337b345
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2015-09-23 10:39:34 +0100

    remove st2 link

--- environment/30_application_links.osx.sh
+++ environment/30_application_links.osx.sh
@@ -2,9 +2,6 @@
 [[ "$OSTYPE" =~ ^darwin ]] || return 1
 
 # link sublimetext user directory to the dotfiles repo
-ln -sfn $HOME/.dotfiles/preferences/sublimetext2/ $HOME/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
-
-# link sublimetext user directory to the dotfiles repo
 ln -sfn $HOME/.dotfiles/preferences/sublimetext3/ $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 
 # link Mou user directory to the dotfiles repo
