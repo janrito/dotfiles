92c80cf8b898f1629fcea5e9e782beb2979a53f2
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2016-04-07 10:28:07 +0100

    homebrew bash completion

--- environment/homebrew.sh
+++ environment/homebrew.sh
@@ -1,12 +1,11 @@
 # OSX-only stuff. Abort if not OSX.
 [[ "$OSTYPE" =~ ^darwin ]] || return 1
 
+
 # Homebrew bash completion
-source `brew --prefix`/Library/Contributions/brew_bash_completion.sh
-
-# generic colourizer
-source "`brew --prefix grc`/etc/grc.bashrc"
-
 if [ -f $(brew --prefix)/etc/bash_completion ]; then
   . $(brew --prefix)/etc/bash_completion
 fi
+
+# generic colourizer
+source "`brew --prefix grc`/etc/grc.bashrc"
