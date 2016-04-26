bdaa38f24a51a351c4bb6cc0cbe245d42adfd019
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2016-04-26 10:21:26 +0100

    apm aliases

--- environment/aliases.sh
+++ environment/aliases.sh
@@ -6,3 +6,13 @@ function wd() {
     git wdiff "$@"
     #git diff --no-index --patience --word-diff=plain "$@"
 }
+
+# apm
+
+function apm_update() {
+    apm install --packages-file ~/.dotfiles/atom/installed-packages.txt
+}
+
+function apm_commit() {
+    apm list --installed --bare > ~/.dotfiles/atom/installed-packages.txt
+}
