aa28d60743aa0bcd3e26b81287f4973cbea0fa4d
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2017-07-18 18:05:00 +0100

    use pip2 explicitly

--- environment/00_functions.sh
+++ environment/00_functions.sh
@@ -5,11 +5,11 @@ function reload_config() {
 }
 
 function _find_powerline_bindings() {
-  # grabs location from pip, which that looks like this:
+  # grabs location from pip2, which that looks like this:
   # Location: /usr/local/lib/python2.7/site-packages
   # if that is where powerline is installed
   # and turns it into an array – split on the space
-  local _powerline_location=($(pip show powerline-status | grep "^Location: .*$" -o))
+  local _powerline_location=($(pip2 show powerline-status | grep "^Location: .*$" -o))
   local _bindings_subdir='powerline/bindings'
   echo ${_powerline_location[1]}'/'$_bindings_subdir
 }
