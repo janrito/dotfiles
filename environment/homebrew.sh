c0d6a953735058a4d84a9f74b64075ea259a8384
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2017-12-19 21:21:25 +0000

    better check for bash completion

--- environment/homebrew.sh
+++ environment/homebrew.sh
@@ -2,10 +2,14 @@ is_osx || return 1
 
 # Homebrew bash completion
 if [ -f $(brew --prefix)/etc/bash_completion ]; then
-  . $(brew --prefix)/etc/bash_completion
+  # default os-x bash 3
+  source $(brew --prefix)/etc/bash_completion
+elif [ -f $(brew --prefix)/share/bash-completion/bash_completion ]; then
+  # homebrew installed bash 4
+  source $(brew --prefix)/share/bash-completion/bash_completion
 fi
 
 # generic colourizer
 if [ -f $(brew --prefix)/etc/grc.bashrc ]; then
-  . $(brew --prefix)/etc/grc.bashrc
+  source $(brew --prefix)/etc/grc.bashrc
 fi
