c8789910baa2261990eda45cdcb803d3e2005407
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2020-03-05 18:08:42 +0000

    comment for colors

--- environment/20_setup.common.sh
+++ environment/20_setup.common.sh
@@ -27,6 +27,9 @@ eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)"
 # Set colors in Command line to true
 CLICOLOR=True
 export CLICOLOR
+
+# generated using https://geoff.greer.fm/lscolors/
+# go look at it
 export LSCOLORS="Gxfxcxdxbxegedabagacad"
 export LS_COLORS="${LS_COLORS}:di=1;36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
 
