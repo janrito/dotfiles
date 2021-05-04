17bed71023d440a694b447639a328cd2663b84a5
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2021-05-04 16:29:51 +0100

    comment on wakeup

--- wakeup.sh
+++ wakeup.sh
@@ -1,3 +1,5 @@
 set -e
 /usr/local/bin/blueutil --power 1
-sleep 2 && /usr/local/bin/blueutil --connect 30-50-75-3c-8c-d5
+
+# connect to headphones
+sleep 2 && /usr/local/bin/blueutil --connect 30-50-75-92-ca-f5
