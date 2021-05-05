922cd1caba497ae64f085fac1b42115f6341c221
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2021-05-05 10:22:17 +0100

    move homebrew conf to homebrew.sh

--- environment/homebrew.sh
+++ environment/homebrew.sh
@@ -1,12 +1,20 @@
 is_osx || return 1
 
-# Homebrew bash completion
-if [ -f $(brew --prefix)/etc/bash_completion ]; then
-  # default os-x bash 3
-  source $(brew --prefix)/etc/bash_completion
-elif [ -f $(brew --prefix)/share/bash-completion/bash_completion ]; then
-  # homebrew installed bash 4
-  source $(brew --prefix)/share/bash-completion/bash_completion
+# homebrew on arm
+if is_arm; then
+  HOMEBREW_PREFIX="/opt/homebrew"
+  eval "$(${HOMEBREW_PREFIX}/bin/brew shellenv)"
+fi
+
+if is_bash; then
+  # Homebrew bash completion
+  if [ -f $(brew --prefix)/etc/bash_completion ]; then
+    # default os-x bash 3
+    source $(brew --prefix)/etc/bash_completion
+  elif [ -f $(brew --prefix)/share/bash-completion/bash_completion ]; then
+    # homebrew installed bash 4
+    source $(brew --prefix)/share/bash-completion/bash_completion
+  fi
 fi
 
 # generic colourizer
