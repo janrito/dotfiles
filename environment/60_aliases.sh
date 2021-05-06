ccd96ddee4c53fdec61350c837cb9b5cca83d58a
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2021-05-06 09:41:36 +0100

    get rid of old alisases

--- environment/60_aliases.sh
+++ environment/60_aliases.sh
@@ -1,21 +1,9 @@
 
+# word diff
 wd() {
     git wdiff "$@"
     #git diff --no-index --patience --word-diff=plain "$@"
 }
 
-## apm
-apm_install_all() {
-    apm install --packages-file ~/.dotfiles/atom/installed-packages.txt
-}
-
-apm_diff() {
-    apm list --installed --bare | grep '^[^@]\+' -o | sort -f | diff - ~/.dotfiles/atom/installed-packages.txt
-}
-
-apm_commit() {
-    apm list --installed --bare | grep '^[^@]\+' -o  | sort -f > ~/.dotfiles/atom/installed-packages.txt
-}
-
-## other
+# other
 alias lss="exa --binary --group --header --links --long --inode --blocks --git"
