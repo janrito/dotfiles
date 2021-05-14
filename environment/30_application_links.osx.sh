7dd68948660f3c22735d6b20e7d541fa0b60069d
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2021-05-14 15:19:27 +0100

    move linking to dotbot

--- environment/30_application_links.osx.sh
+++ environment/30_application_links.osx.sh
@@ -1,11 +1,4 @@
 is_interactive && is_osx || return 1
 
-# link subl utilty
-if [[ -e "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ]]; then
-    ln -sfn "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" $HOME/.bin/subl
-fi
 
-if [[ -e "/Applications/Sublime Merge.app/Contents/SharedSupport/bin/smerge" ]]; then
-    ln -sfn "/Applications/Sublime Merge.app/Contents/SharedSupport/bin/smerge" $HOME/.bin/smerge
-fi
 
