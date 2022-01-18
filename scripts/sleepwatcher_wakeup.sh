0924bb916224c4c14667905fafd40dd927c5d10c
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2022-01-18 12:13:10 +0000

    safer scripts interpolation of profile

--- scripts/sleepwatcher_wakeup.sh
+++ scripts/sleepwatcher_wakeup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # needs path configuration
-source ~/.dotfiles/profile
+# shellcheck source=profile
+source "$HOME/.dotfiles/profile"
 
 echo "SLEEPWATCHER $(date +%Y-%m-%dT%H:%M:%S%z): wakeup"
 
