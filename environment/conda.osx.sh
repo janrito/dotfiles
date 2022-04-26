6b5e0e6dde595971ca5423c4e9ee403ffb876de4
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2022-04-26 23:37:03 +0100

    conda setup needs to check the exit code

--- environment/conda.osx.sh
+++ environment/conda.osx.sh
@@ -4,7 +4,9 @@ is_osx && is_zsh || return 1
 
 __conda_setup="$("${HOMEBREW_PREFIX}/Caskroom/miniforge/base/bin/conda" 'shell.zsh' 'hook' 2>/dev/null)"
 
-if [ "$__conda_setup" ]; then
+# shellcheck disable=SC2181 # we need to check for the exit code, not the output
+if [ $? -eq 0 ]; then
+  _debug "conda setup"
   eval "$__conda_setup"
 else
   if [ -f "${HOMEBREW_PREFIX}/Caskroom/miniforge/base/etc/profile.d/conda.sh" ]; then
