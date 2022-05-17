3045ac7ee5b8a693a808bb5838d9b365ebb52d9e
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2022-05-17 09:58:01 +0100

    mov_to_gif function

--- environment/aliases.sh
+++ environment/aliases.sh
@@ -11,3 +11,22 @@ alias lss="exa --binary --group --header --links --long --inode --blocks --git"
 
 # code insiders short
 alias ci="code-insiders"
+
+# make gifs
+mov_to_gif() {
+  tmp_palette=$(mktemp /tmp/mov_to_gif_palette.png)
+  tmp_gif=$(mktemp /tmp/mov_to_gif_palette.gif)
+
+  success=$(ffmpeg -i "$1" -filter_complex "[0:v] palettegen" -y "$tmp_palette" &&
+    ffmpeg -i "$1" -i "$tmp_palette" -filter_complex "[0:v][1:v] paletteuse" -f gif -y "$tmp_gif" &&
+    gifsicle -O3 "$tmp_gif" -o "${1%.*}.gif")
+
+  rm "$tmp_palette" "$tmp_gif"
+
+  if [ "$success" = "0" ]; then
+    echo "${1%.*}.gif successfully converted and saved"
+  else
+    echo "Failed to convert $1 into gif"
+  fi
+
+}
